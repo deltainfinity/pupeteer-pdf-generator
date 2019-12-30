a7a082adea6e5638559cb9414b19576aa1f4fa3d
Jamie Newton <jnewton@vant4ge.com>
2019-12-30 16:58:56 -0700

    fixed props for data to be passed through

--- controller/safeToPdf.js
+++ controller/safeToPdf.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const saveToPdf = async () => {
   // Browser actions & buffer creator
   const browser = await puppeteer.launch({
+
+    // devtools: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'] // SEE BELOW WARNING!!!
   })
   const page = await browser.newPage()
@@ -14,8 +16,41 @@ const saveToPdf = async () => {
   var html = `
   <html>
   <body>
-  <report-header report-Name="assessment" :user="{'SubjectLastName':'dude', 'SubjectFirstName':'the'}"></report-header>
+   
+  <report-header report-Name="assessment" user-Data='{ "SubjectLastName": "dude", "SubjectFirstName": "the" }'></report-header>
+  <report-demographics  user-Data='{ "SubjectExternalID": 123, "SORID": "345", "BirthDate":"02/14/1990", "UserLastName": "dude", "UserFirstName": "the" }'></report-demographics>
+ 
+  <report-domain-header domain-Header-Name="domain 1">
+    <report-section-header section-Header-Name="section 1"></report-section-header>
+    <report-question-with-single-answers data='{"QuestionSequence":1,"QuestionText":"Question 1", "Answers":[{"AnswerValue":"Answer 1","AnswerImageType":3}]}'></report-question-with-single-answers>
+    <report-question-with-single-answers data='{"QuestionSequence":2,"QuestionText":"Question 2", "Answers":[{"AnswerValue":"Answer 2","AnswerImageType":4}]}'></report-question-with-single-answers>
+    <report-question-with-single-answers data='{"QuestionSequence":3,"QuestionText":"Question 3", "Answers":[{"AnswerValue":"Answer 3","AnswerImageType":5}]}'></report-question-with-single-answers>
+    <report-question-with-single-answers data='{"QuestionSequence":4,"QuestionText":"Question 4", "Answers":[{"AnswerValue":"Answer 4","AnswerImageType":6}]}'></report-question-with-single-answers>
+    <report-section-header section-Header-Name="section 2"></report-section-header>
+    <report-question-with-single-answers data='{"QuestionSequence":1,"QuestionText":"Question 1", "Answers":[{"AnswerValue":"Answer 1","AnswerImageType":3}]}'></report-question-with-single-answers>
+    <report-question-with-single-answers data='{"QuestionSequence":2,"QuestionText":"Question 2", "Answers":[{"AnswerValue":"Answer 2","AnswerImageType":4}]}'></report-question-with-single-answers>
+    <report-question-with-single-answers data='{"QuestionSequence":3,"QuestionText":"Question 3", "Answers":[{"AnswerValue":"Answer 3","AnswerImageType":5}]}'></report-question-with-single-answers>
+    <report-question-with-single-answers data='{"QuestionSequence":4,"QuestionText":"Question 4", "Answers":[{"AnswerValue":"Answer 4","AnswerImageType":6}]}'></report-question-with-single-answers>
+    
+  </report-domain-header>
+
+  <report-domain-header domain-Header-Name="domain 2">
+  <report-section-header section-Header-Name="section 1"></report-section-header>
+  <report-question-with-single-answers data='{"QuestionSequence":1,"QuestionText":"Question 1", "Answers":[{"AnswerValue":"Answer 1","AnswerImageType":3}]}'></report-question-with-single-answers>
+  <report-question-with-single-answers data='{"QuestionSequence":2,"QuestionText":"Question 2", "Answers":[{"AnswerValue":"Answer 2","AnswerImageType":4}]}'></report-question-with-single-answers>
+  <report-question-with-single-answers data='{"QuestionSequence":3,"QuestionText":"Question 3", "Answers":[{"AnswerValue":"Answer 3","AnswerImageType":5}]}'></report-question-with-single-answers>
+  <report-question-with-single-answers data='{"QuestionSequence":4,"QuestionText":"Question 4", "Answers":[{"AnswerValue":"Answer 4","AnswerImageType":6}]}'></report-question-with-single-answers>
+  
+  <report-section-header section-Header-Name="section 2"></report-section-header>
+  <report-question-with-single-answers data='{"QuestionSequence":1,"QuestionText":"Question 1", "Answers":[{"AnswerValue":"Answer 1","AnswerImageType":3}]}'></report-question-with-single-answers>
+  <report-question-with-single-answers data='{"QuestionSequence":2,"QuestionText":"Question 2", "Answers":[{"AnswerValue":"Answer 2","AnswerImageType":4}]}'></report-question-with-single-answers>
+  <report-question-with-single-answers data='{"QuestionSequence":3,"QuestionText":"Question 3", "Answers":[{"AnswerValue":"Answer 3","AnswerImageType":5}]}'></report-question-with-single-answers>
+  <report-question-with-single-answers data='{"QuestionSequence":4,"QuestionText":"Question 4", "Answers":[{"AnswerValue":"Answer 4","AnswerImageType":6}]}'></report-question-with-single-answers>
+  
+  </report-domain-header>
+
   <report-footer></report-footer>
+ 
   </body>
   </html>`
   await page.setContent(html)
